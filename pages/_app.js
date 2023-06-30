5c5e77106dd80aaa3b75f585158e8e1e1f8313f5
harii84 <99866727+harii84@users.noreply.github.com>
2023-06-30 22:02:11 +0530

    add loader

--- pages/_app.js
+++ pages/_app.js
@@ -4,22 +4,52 @@ import { SessionProvider, useSession } from 'next-auth/react';
 import { StoreProvider } from '../utils/Store';
 import { useRouter } from 'next/router';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import NextNProgress from 'nextjs-progressbar';
+function Loading() {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url) => (url !== router.asPath) && setLoading(true);
+    const handleComplete = (url) => (url === router.asPath) && setLoading(false);
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  })
+
+  return loading && (<div>Loading....{/*I have an animation here*/}</div>);
+}
+
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <StoreProvider>
-        <PayPalScriptProvider deferLoading={true}>
-          {Component.auth ? (
-            <Auth adminOnly={Component.auth.adminOnly}>
+    <>
+      <NextNProgress
+        color="#002222"
+        height="8"
+      />
+      <SessionProvider session={session}>
+        <StoreProvider>
+          <PayPalScriptProvider deferLoading={true}>
+            {Component.auth ? (
+              <Auth adminOnly={Component.auth.adminOnly}>
+                <Component {...pageProps} />
+              </Auth>
+            ) : (
               <Component {...pageProps} />
-            </Auth>
-          ) : (
-            <Component {...pageProps} />
-          )}
-        </PayPalScriptProvider>
-      </StoreProvider>
-    </SessionProvider>
+            )}
+          </PayPalScriptProvider>
+        </StoreProvider>
+      </SessionProvider>
+    </>
   );
 }
 
